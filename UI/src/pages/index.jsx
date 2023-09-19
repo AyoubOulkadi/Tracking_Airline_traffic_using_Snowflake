c0940b9759bedae6beaad8404a042001b6717892
mehdi bahou <bahoubuisness@gmail.com>
2023-09-19 22:10:27 +0100

    UI integration with api debuging

---
+++ UI/src/pages/index.jsx
@@ -0,0 +1,286 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const API_URL = 'http://localhost:5000'; // Replace with your Flask API base URL
+
+const airlineOptions = [
+  'JetBlue Airways',
+  'Delta Air Lines',
+  'Cathay Pacific',
+  'ANA - All Nippon Airways',
+  'China Eastern Airlines',
+  'Etihad Airways',
+  'Ryanair',
+  'Qatar Airways',
+  'Qantas',
+  'United Airlines',
+  'Alitalia',
+  'American Airlines',
+  'Emirates',
+  'Japan Airlines',
+  'Turkish Airlines',
+  'Singapore Airlines',
+  'Air Canada',
+  'Finnair',
+  'Virgin Atlantic',
+  'Royal Air Maroc',
+  'KLM Royal Dutch Airlines',
+  'Southwest Airlines',
+  'Air France',
+  'Lufthansa',
+  'British Airways',
+  'AirAsia',
+];
+
+const seasonOptions = ['Summer', 'Spring', 'Winter'];
+
+export default function Home() {
+  const [data, setData] = useState([]);
+  const [filters, setFilters] = useState({});
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+
+  const fetchTotalPages = async () => {
+    try {
+      const response = await axios.get(`${API_URL}/get_total_pages`, {
+        params: filters,
+      });
+      const { total_pages: total } = response.data; // Assuming the API returns total_pages
+
+      setTotalPages(total);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(`${API_URL}/get_data`, {
+        params: { ...filters, page },
+      });
+      console.log(response);
+      const data = response.data;
+
+      setData(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [page]);
+
+  useEffect(() => {
+    fetchTotalPages();
+  }, [filters]);
+
+  const handleFilterChange = (e) => {
+    const { name, value } = e.target;
+    setFilters({ ...filters, [name]: value });
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    fetchData();
+  };
+
+  const handlePageChange = (newPage) => {
+    setPage(newPage);
+  };
+
+  // Dropdown component for airline options
+  const AirlineDropdown = () => (
+    <select
+      id="aller_type"
+      name="aller_type"
+      className="form-input rounded-md px-2 py-1 w-1/2 mx-auto"
+      onChange={handleFilterChange}
+      value={filters.aller_type || ''}
+    >
+      <option value="">Select Airline Type</option>
+      {airlineOptions.map((option) => (
+        <option key={option} value={option}>
+          {option}
+        </option>
+      ))}
+    </select>
+  );
+
+  // Dropdown component for return options
+  const ReturnDropdown = () => (
+    <select
+      id="return_type"
+      name="return_type"
+      className="form-input rounded-md px-2 py-1 w-1/2 mx-auto"
+      onChange={handleFilterChange}
+      value={filters.return_type || ''}
+    >
+      <option value="">Select Return Type</option>
+      {airlineOptions.map((option) => (
+        <option key={option} value={option}>
+          {option}
+        </option>
+      ))}
+    </select>
+  );
+
+  // Dropdown component for season options
+  const SeasonDropdown = () => (
+    <select
+      id="season"
+      name="season"
+      className="form-input rounded-md px-2 py-1 w-1/2 mx-auto"
+      onChange={handleFilterChange}
+      value={filters.season || ''}
+    >
+      <option value="">Select Season</option>
+      {seasonOptions.map((option) => (
+        <option key={option} value={option}>
+          {option}
+        </option>
+      ))}
+    </select>
+  );
+
+  // Pagination component with dynamic display
+  const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+    const displayPages = 10; // Number of pages to display at once
+    const pageLinks = [];
+
+    // Calculate the range of pages to display
+    let startPage = Math.max(1, currentPage - Math.floor(displayPages / 2));
+    let endPage = startPage + displayPages - 1;
+
+    // Ensure that the endPage does not exceed the total number of pages
+    if (endPage > totalPages) {
+      endPage = totalPages;
+      startPage = Math.max(1, endPage - displayPages + 1);
+    }
+
+    // Add ellipsis points if there are more pages before or after
+    if (startPage > 1) {
+      pageLinks.push(
+        <button key="start" onClick={() => onPageChange(1)} className="mx-1">
+          1
+        </button>
+      );
+      if (startPage > 2) {
+        pageLinks.push(<span key="ellipsis-start">...</span>);
+      }
+    }
+
+    for (let i = startPage; i <= endPage; i++) {
+      pageLinks.push(
+        <button
+          key={i}
+          onClick={() => onPageChange(i)}
+          className={`mx-1 ${
+            currentPage === i ? 'bg-blue-500 px-1 rounded text-white' : ''
+          }`}
+        >
+          {i}
+        </button>
+      );
+    }
+
+    if (endPage < totalPages) {
+      if (endPage < totalPages - 1) {
+        pageLinks.push(<span key="ellipsis-end">...</span>);
+      }
+      pageLinks.push(
+        <button
+          key="end"
+          onClick={() => onPageChange(totalPages)}
+          className="mx-1"
+        >
+          {totalPages}
+        </button>
+      );
+    }
+
+    return <div className="flex justify-center space-x-4 mt-4">{pageLinks}</div>;
+  };
+
+  return (
+    <div className="bg-gray-200 min-h-screen p-4">
+      <div className="bg-white rounded-lg shadow-md p-4 mb-6">
+        <h1 className="text-3xl font-bold mb-4">Airline Data</h1>
+        <form onSubmit={handleFormSubmit}>
+          <div className="grid grid-cols-1 gap-4">
+            <div className="form-group flex items-center">
+              <AirlineDropdown />
+            </div>
+            <div className="form-group flex items-center">
+              <ReturnDropdown />
+            </div>
+            <div className="form-group flex items-center">
+              <input
+                type="number"
+                id="min_price"
+                name="min_price"
+                className="form-input rounded-md px-3 py-2 w-1/2 mx-auto"
+                placeholder="Min Price"
+                onChange={handleFilterChange}
+                value={filters.min_price || ''}
+              />
+            </div>
+            <div className="form-group flex items-center">
+              <input
+                type="number"
+                id="max_price"
+                name="max_price"
+                className="form-input rounded-md px-3 py-2 w-1/2 mx-auto"
+                placeholder="Max Price"
+                onChange={handleFilterChange}
+                value={filters.max_price || ''}
+              />
+            </div>
+            <div className="form-group flex items-center">
+              <SeasonDropdown />
+            </div>
+            <div className="form-group flex items-center">
+              <button
+                type="submit"
+                className="bg-blue-500 text-white rounded-md px-4 py-2 hover:bg-blue-600 w-1/2 mx-auto"
+              >
+                Submit
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+      <div className="bg-white rounded-lg shadow-md p-4">
+        <table className="table-auto w-full ">
+          <thead>
+            <tr>
+              <th className="px-4 py-2 ">Airline Aller</th>
+              <th className="px-4 py-2 ">Airline Retour</th>
+              <th className="px-4 py-2 ">Price</th>
+              <th className="px-4 py-2 ">Arrivé</th>
+              <th className="px-4 py-2 ">Départ</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data?.map((row, index) => (
+              <tr key={index} className='even:bg-slate-100'>
+                <td className="px-4 py-2">{row.AIRLINE_ALLER}</td>
+                <td className="px-4 py-2">{row.AIRLINE_RETOUR}</td>
+                <td className="px-4 py-2">{row.PRICE}</td>
+                <td className="px-4 py-2">{row.HOR_ARRI}</td>
+                <td className="px-4 py-2">{row.HOR_DEP}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div className="mt-4">
+          <Pagination
+            currentPage={page}
+            totalPages={totalPages}
+            onPageChange={handlePageChange}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
--- snowflake_API.py
+++ snowflake_API.py
@@ -1,7 +1,6 @@
 from flask import Flask, jsonify, request
 import snowflake.connector
-
-# Snowflake connection parameters
+from flask_cors import CORS
 snowflake_config = {
     'account': 'rsrnnfw-hr03420',
     'warehouse': 'COMPUTE_WH',
@@ -12,12 +11,10 @@ snowflake_config = {
     'password': 'Ma123456789@A'
 }
 
-# Initialize Flask app
 app = Flask(__name__)
-
-# Define a route to fetch data from Snowflake with filtering options
-@app.route('/get_data', methods=['GET'])
-def get_data():
+CORS(app, resources={r"/*": {"origins": "*"}})
+@app.route('/get_total_pages', methods=['GET'])
+def get_total_pages():
     try:
         # Establish a connection to Snowflake
         connection = snowflake.connector.connect(**snowflake_config)
@@ -25,16 +22,17 @@ def get_data():
         # Create a cursor object to execute SQL queries
         cursor = connection.cursor()
 
-        # Build the SQL query based on client preferences
-        query = "SELECT * FROM Airline_sample_table WHERE 1=1"
+        # Build the SQL query to count the total number of pages
+        query = "SELECT COUNT(*) FROM Airline_sample_table WHERE 1=1"
 
         aller_type = request.args.get('aller_type')
+        
         if aller_type:
-            query += f" AND ALLER_TYPE = '{aller_type}'"
+            query += f" AND AIRLINE_ALLER = '{aller_type}'"
 
         return_type = request.args.get('return_type')
         if return_type:
-            query += f" AND RETURN_TYPE = '{return_type}'"
+            query += f" AND AIRLINE_RETOUR = '{return_type}'"
 
         min_price = request.args.get('min_price')
         if min_price:
@@ -48,46 +46,14 @@ def get_data():
         if season:
             query += f" AND SEASON = '{season}'"
 
-        # You can add more filter conditions as needed based on client preferences
-
-        # Execute the final SQL query
         cursor.execute(query)
+        total_records = cursor.fetchone()[0]
+        total_pages = (total_records + 9) // 10  # Assuming 10 records per page
 
-        # Fetch the results
-        results = cursor.fetchall()
-    
-        # Close the cursor and connection
         cursor.close()
         connection.close()
 
-        # Convert the results to a list of dictionaries for JSON serialization
-        L = []
-        
-        for row in results:
-            L.append({
-                "AIRLINE_ALLER": str(row[0]),
-                "AIRLINE_RETOUR": str(row[1]),
-                "ALLER_TYPE": str(row[2]),
-                "ARRIVAL_DAY_TYPE": str(row[3]),
-                "CITY_ARRIVAL": str(row[4]),
-                "CITY_DEPARTURE": str(row[5]),
-                "DATE_DE_DEPART": str(row[6]),  # Date is already a strin)g
-                "DATE_DE_RETOUR": str(row[7]),  # Date is already a strin)g
-                "DEPARTURE_DAY_TYPE": str(row[8]),
-                "FLIGHT_DURATION_DEPARTURE": str(row[9]),
-                "FLIGHT_DURATION_RETURN": str(row[10]),
-                "FLIGHT_ROUTE": str(row[11]),
-                "HOR_ARRI": str(row[12]),  # Time is already a strin)g
-                "HOR_DEP": str(row[13]),   # Time is already a strin)g
-                "HOR_DEP_RETOUR": str(row[14]),  # Time is already a strin)g
-                "HOR_RETOUR": str(row[15]),      # Time is already a strin)g
-                "PRICE": str(row[16]),
-                "PRICE_CATEGORY": str(row[17]),
-                "RETURN_TYPE": str(row[18]),
-                "SEASON": str(row[19])
-            })
-
-        return jsonify(L), 200
+        return jsonify({'total_pages': total_pages}), 200
 
     except Exception as e:
         print("Error:", str(e))  # Print the error message
@@ -95,6 +61,79 @@ def get_data():
         traceback.print_exc()  # Print the traceback for more details
         return jsonify({'error': str(e)}), 500
 
+@app.route('/get_data', methods=['GET'])
+def get_data():
+    try:
+        connection = snowflake.connector.connect(**snowflake_config)
+        cursor = connection.cursor()
+        query = "SELECT * FROM Airline_sample_table WHERE 1=1"
+
+        aller_type = request.args.get('aller_type')
+        if aller_type:
+            query += f" AND AIRLINE_ALLER = '{aller_type}'"
+
+        return_type = request.args.get('return_type')
+        if return_type:
+            query += f" AND AIRLINE_RETOUR = '{return_type}'"
+
+        min_price = request.args.get('min_price')
+        if min_price:
+            query += f" AND PRICE >= {min_price}"
+
+        max_price = request.args.get('max_price')
+        if max_price:
+            query += f" AND PRICE <= {max_price}"
+
+        season = request.args.get('season')
+        if season:
+            query += f" AND SEASON = '{season}'"
+
+        page = request.args.get('page', default=1, type=int)
+        per_page = request.args.get('per_page', default=10, type=int)
+        offset = (page - 1) * per_page
+        query += f" OFFSET {offset} ROWS FETCH NEXT {per_page} ROWS ONLY"
+        print(query)
+        cursor.execute(query)
+
+        results = cursor.fetchall()
+
+        cursor.close()
+        connection.close()
+
+        L = []
+
+        for row in results:
+            row_dict = {
+                "AIRLINE_ALLER": str(row[0]),
+                "AIRLINE_RETOUR": str(row[1]),
+                "HOR_DEP": str(row[2]),
+                "HOR_ARRI": str(row[3]),
+                "PRICE": str(row[4]),
+                "ALLER_TYPE": str(row[5]),
+                "RETURN_TYPE": str(row[6]),  
+                "DATE_DE_DEPART": str(row[7]),  
+                "DATE_DE_RETOUR": str(row[8]),
+                "CITY_DEPARTURE": str(row[9]),
+                "CITY_ARRIVAL": str(row[10]),
+                "HOR_DEP_RETOUR": str(row[11]),
+                "HOR_RETOUR": str(row[12]),  
+                "FLIGHT_DURATION_DEPARTURE": str(row[13]),   
+                "FLIGHT_DURATION_RETURN": str(row[14]),  
+                "SEASON": str(row[15]),      
+                "DEPARTURE_DAY_TYPE": str(row[16]),
+                "ARRIVAL_DAY_TYPE": str(row[17]),
+                "PRICE_CATEGORY": str(row[18]),
+                "FLIGHT_ROUTE": str(row[19])
+            }
+            L.append(row_dict)
+
+        return jsonify(L), 200
+
+    except Exception as e:
+        print("Error:", str(e))
+        import traceback
+        traceback.print_exc()
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
